342250890ae681d18a1274359a4a7a7ea0296631
Tom <Thomasvill16@inbox.ru>
2024-03-26 13:31:55 +0400

    Initial commit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $ru.tyga;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
